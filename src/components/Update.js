01cd6f7b47fa2c82410b18fc71a89f4635b2f394
Katri Nousiainen <katrinousiainen.kn@gmail.com>
2019-11-20 19:34:17 +0200

    Update sivu lisätty

---
+++ src/components/Update.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+// import '../toiminnallisuudet/LuoTuote';
+import MuokkaaTuote from '../toiminnallisuudet/MuokkaaTuote';
+import Edit from "../toiminnallisuudet/MuokkaaTuote";
+import PoistaTuote from "../toiminnallisuudet/PoistaTuote";
+
+ 
+class Update extends Component {
+    render() {
+
+     return (
+         <div className="container">
+        <div className="update">
+          <h3 className="center"></h3>
+          <div className="box"></div>
+          <p>Muokkaa ilmoitusta:</p>
+          {/* <div><MuokkaaTuote/></div> tai <div><Edit/></div> riippuen tietty mitä MuokkaaTuote palauttaa?*/}
+          {/* <div><PoistaTuote/></div> */}
+          
+          </div>
+          </div>
+        )
+    }
+  
+}
+ 
+export default Update;
