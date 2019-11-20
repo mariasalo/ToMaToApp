650ea7a057810681b7ff46a3136d082241d52ad1
eveliinaj <eveliina.jarvelainen@gmail.com>
2019-11-20 16:34:12 +0200

    muutettu toiminnallisuuksia, korjattu datan näyttäminen tietokannasta ja poistettu osa redux toiminnallisuuksista etusivulta

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,21 @@ import HaeTuote from '../toiminnallisuudet/HaeTuote';
 //import talletaTuotteet from '..toiminnallisuudet/TuoteService.js'; // ei toimi
 import getKaikki from '../toiminnallisuudet/tuoteService';
 import LuoTuote from '../toiminnallisuudet/LuoTuote';
+import Tuote from '../toiminnallisuudet/Tuote';
 
  class Home extends Component{
 //nämä kuntoon:-----
     constructor(props) {  
         super(props);  
         //this.state = 
-        this.state = {tuotteet: []};  
+        this.state = {tuotteet: [
+            {id:1,title:'Kurpitsakeitto', desc: "Keilaniemi, Espoo", price:110},
+            {id:2,title:'Luomukananmunia', desc: "Kallio, Helsinki", price:80},
+            {id:3,title:'Banaaneja', desc: "Punavuori, Helsinki",price:120},
+            {id:4,title:'Riisiä', desc: "Otaniemi, Espoo", price:260},
+            {id:5,title:'Leivonnaiset', desc: "Lehtisaari, Helsinki", price:160},
+            {id:6,title:'Tomaatteja', desc: "Ruoholahti, Helsinki",price:90}
+        ],};  
       }  
       componentDidMount(){  
         // debugger;  
@@ -26,15 +34,19 @@ import LuoTuote from '../toiminnallisuudet/LuoTuote';
     
     handleClick = (id)=>{
         this.props.addToCart(id); 
+        //oma .js luokka addToCart sijaan jolle välitetään parametri
     }
 
     render(){
 
-        let itemList = this.props.items.map(item=>{
+                let itemList = this.props.items.map(item=>{
             return(
                 <div className="card" key={item.id}>
                         <div className="card-image">
                             <img src={item.img} alt={item.title}/>
+
+
+                            
                             <span className="card-title"></span>
                             <span to="/" className="btn-floating halfway-fab waves-effect waves-light red" onClick={()=>{this.handleClick(item.id)}}><i className="material-icons">add</i></span>
                         </div>
@@ -44,25 +56,29 @@ import LuoTuote from '../toiminnallisuudet/LuoTuote';
                             <p>{item.desc}</p>
                         </div>
                  </div>
-
             )
         })
 
-   
-        {/* <li><Link to="/location"><i className="material-icons">room</i></Link></li> */}
-        {/* <li><Link to="/view"><i className="material-icons">view_module</i></Link></li> */}
+        let itemList2 = this.state.tuotteet.map(item=>{
+            return(
+                <Tuote key={item.id} data={item}>
 
+                        
+                 </Tuote>
+        
+            );
+        })
 
         return(
             <div className="container">
                 <h3 className="center"></h3>
                 <div className="box">
                     {itemList}
-                </div>
+                    {itemList2}
+
                 </div>
 
-                <div><HaeTuote tuotteet={this.state.tuotteet}/></div>
-                <div><LuoTuote/></div>
+                {/* <div><HaeTuote tuotteet={this.state.tuotteet}/></div> */}
             </div>
         )
     }
